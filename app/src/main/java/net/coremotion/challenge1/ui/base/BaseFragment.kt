47fd8cc074b180fe1f785d5eb18308ec8b85ee0c
Luka <nikuradzeluka0@gmail.com>
2022-08-31 11:53:52 +0400

    commit

---
+++ app/src/main/java/net/coremotion/challenge1/ui/base/BaseFragment.kt
@@ -0,0 +1,39 @@
+package net.coremotion.challenge1.ui.base
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+abstract class BaseFragment<BD : ViewBinding>(private val inflate: Inflate<BD>) : Fragment() {
+
+    private var _binding: BD? = null
+    protected val binding: BD get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        start()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+
+    abstract fun start(
+    )
+}
---
+++ app/src/main/java/net/coremotion/challenge1/ui/users/UsersViewModel.kt
@@ -0,0 +1,32 @@
+package net.coremotion.challenge1.ui.users
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.flow.Flow
+import net.coremotion.challenge1.domain.model.Users
+import net.coremotion.challenge1.domain.reposoitory.UserRepository
+import net.coremotion.challenge1.ui.users.source.UsersPagingSource
+import javax.inject.Inject
+
+@HiltViewModel
+class UsersViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+
+
+    fun usersFlow(): Flow<PagingData<Users.Data>> =
+        Pager(
+            config = PagingConfig(pageSize = 1,maxSize = 200),
+            pagingSourceFactory = { UsersPagingSource(userRepository) })
+            .flow.cachedIn(viewModelScope)
+
+
+}
